9aeb73c4136638046349b8aa795d4a80ca815942
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-06-14 16:33:38 +0200

    Riscrittura commenti

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const gameDifficulty = parseInt(
   prompt("Scegli un grado di difficoltà da 1 a 3")
 );
-// Determino il range di numeri in base alla difficoltà scelta
+// Determino il range di numeri in base alla difficoltà scelta e dichiaro il numero di bombe presenti
 const minRange = 1;
 let maxRange;
 
@@ -14,6 +14,43 @@ if (gameDifficulty === 1) {
   maxRange = 49;
 }
 const bombNumber = 16;
+
+// Invoco la funzione di creazione delle bombe e inserisco il risultato in una variabile
+bombGenerator(bombNumber, minRange, maxRange);
+let bombArray = bombGenerator(bombNumber, minRange, maxRange);
+
+// Creo un array dove inserire i numeri inseriti dall'utente, se rispettano le condizioni, e interrompo il gioco qualora trovasse un numero bomba. Dichiaro la variabile di continuazione del gioco, dandole come valore iniziale true
+let gameContinues = true;
+let userArray = [];
+const winningScore = maxRange - minRange - bombNumber + 1;
+
+// Imposto un ciclo while per l'inserimento dei numeri seconda le condizioni scelte
+while ((gameContinues = true)) {
+  // Controllo se la condizione di vittoria è stata raggiunta
+  if (userArray.length === winningScore) {
+    alert("Congratulazioni!! Hai vinto!!");
+    gameContinues = false;
+    break;
+  }
+  // Chiedo all'utente un numero
+  let userNumber = parseInt(
+    prompt(`Scegli un numero compreso tra ${minRange} e ${maxRange}`)
+  );
+  // Inserico il numero nell'array dei numeri utente se questo non è presente sia nell'array delle bombe sia in quello utente
+  if (bombArray.includes(userNumber)) {
+    alert(`BOOOOM!!! Hai Perso!! Il tuo punteggio è: ${userArray.length}`);
+    gameContinues = false;
+    break;
+  } else if (!userArray.includes(userNumber)) {
+    userArray.push(userNumber);
+    console.log(userArray);
+  }
+}
+
+// -----------------------
+// --------Funzioni-------
+// -----------------------
+
 // Imposto una funzione che crei un array di numeri bomba, in base al numero di bombe e range di numeri
 const bombGenerator = function (bombNumber, minRange, maxRange) {
   let bombArray = [];
@@ -28,32 +65,3 @@ const bombGenerator = function (bombNumber, minRange, maxRange) {
   }
   return bombArray;
 };
-// Invoco la funzione di creazione delle bombe e inserisco il risultato in una variabile
-bombGenerator(bombNumber, minRange, maxRange);
-let bombArray = bombGenerator(bombNumber, minRange, maxRange);
-console.log(bombArray);
-
-// Creo un array dove inserire i numeri inseriti dall'utente, se rispettano le condizioni, e interrompo il gioco qualora trovasse un numero bomba
-let gameContinues = true;
-let userArray = [];
-const winningScore = maxRange - minRange - bombNumber + 1;
-while ((gameContinues = true)) {
-  if (userArray.length === winningScore) {
-    alert("Congratulazioni!! Hai vinto!!");
-    gameContinues = false;
-    break;
-  }
-  // Chiedo all'utente un numero
-  let userNumber = parseInt(
-    prompt(`Scegli un numero compreso tra ${minRange} e ${maxRange}`)
-  );
-
-  if (bombArray.includes(userNumber)) {
-    alert(`BOOOOM!!! Hai Perso!! Il tuo punteggio è: ${userArray.length}`);
-    gameContinues = false;
-    break;
-  } else if (!userArray.includes(userNumber)) {
-    userArray.push(userNumber);
-    console.log(userArray);
-  }
-}
